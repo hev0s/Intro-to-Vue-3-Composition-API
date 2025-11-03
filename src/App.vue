d1c46484d66a79da29952fbc0777206d30c00d7c
hev0s <niels.delafontaine@eduvaud.ch>
2025-11-03 15:25:47 +0100

    Feat:Ex4

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup>
 import { ref } from 'vue'
 import socksGreenImage from './assets/images/socks_green.jpeg'
-
+import socksBlueImage from './assets/images/socks_blue.jpeg'
 const product = ref('Socks')
+const inStock = false
+const onSale = false
+const inventory = 1
 const image = ref(socksGreenImage)
 
 </script>
@@ -16,6 +19,12 @@ const image = ref(socksGreenImage)
       </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
+        <p v-if="inStock">En stock</p>
+        <p v-else>En rupture de stock</p>
+        <p v-if="inventory > 10">En stock</p>
+        <p v-else-if="inventory >= 1">Faible stock</p>
+        <p v-else-if="inventory >= 0">En rupture de stock</p>
+        <p v-if="onSale">En solde</p>
       </div>
     </div>
   </div>
