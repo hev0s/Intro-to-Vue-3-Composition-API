acc89d2d45566ae0d2d29b0274e9a6110726e800
hev0s <niels.delafontaine@eduvaud.ch>
2025-11-03 14:51:04 +0100

    Feat:Ex2

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+const product = ref("Chaussettes")
+const description = ref("Chaussettes confort premium pour l’entraînement (matière respirante).")
+const image = ref("./src/assets/images/socks_blue.jpeg")
+const url = ref("https://www.cpnv.ch/")
+const linkLabel = ref("Voir plus d’infos (CPNV)")
+const inStock = ref(false)
+const inventory = ref(11)
+const onSale = ref(true)
+</script>
 
 <template>
   <div class="product-display">
     <div class="product-container">
       <div class="product-info">
-        <h1>Socks</h1>
+        <h1>{{product}}</h1>
       </div>
+      <div class="product-image">
+        <img src="./assets/images/socks_blue.jpeg">
+      </div>
+      <div class="product-description">
+        <p>{{description}}</p>
+      </div>
+    </div>
+    <div class="link">
+      <a :href="url" target="_blank" rel="noopener noreferrer">
+        <link rel="stylesheet" href="https://www.cpnv.ch/">https://www.cpnv.ch/
+      </a>
     </div>
   </div>
 </template>
