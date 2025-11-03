0d4061f478328829c0dea863d34a4b678fd23164
hev0s <niels.delafontaine@eduvaud.ch>
2025-11-03 14:55:11 +0100

    Feat:Ex3

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <script setup>
 import { ref } from 'vue'
-
 const product = ref('Socks')
-
+const image = ref("./src/assets/images/socks_blue.jpeg")
 </script>
 
 <template>
   <div class="product-display">
     <div class="product-container">
       <div class="product-image">
-        <!-- image goes here -->
+        <img src="./assets/images/socks_blue.jpeg">
       </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
