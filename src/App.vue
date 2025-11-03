fd000ae62f30aaacbbc3c3dc8211d4438c2a9a27
hev0s <niels.delafontaine@eduvaud.ch>
2025-11-03 15:02:55 +0100

    Feat:Ex3

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
 import { ref } from 'vue'
 const product = ref('Socks')
+const description = ref("Chaussettes confort premium")
 const image = ref("./src/assets/images/socks_blue.jpeg")
+const url = ref("https://www.cpnv.ch/")
+const linkLabel = ref("Voir plus d’infos (CPNV)")
 </script>
 
 <template>
@@ -12,6 +15,12 @@ const image = ref("./src/assets/images/socks_blue.jpeg")
       </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
+        <div class="product-description">
+          <p>{{ description }}</p>
+        </div>
+      </div>
+      <div class="product-links">
+        <p>{{ url }}</p>
       </div>
     </div>
   </div>
