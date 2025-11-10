c7688c1ae817a00a1a38b76af35408749deea7ec
hev0s <niels.delafontaine@eduvaud.ch>
2025-11-10 10:41:53 +0100

    Feature:App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {ref, computed} from 'vue'
+import { ref, computed } from 'vue'
 import socksGreenImage from './assets/images/socks_green.jpeg'
 import socksBlueImage from './assets/images/socks_blue.jpeg'
 
@@ -11,8 +11,6 @@ const brand = ref('Vue Mastery')
 
 const selectedVariant = ref(0)
 
-const cart = ref(0)
-
 const details = ref(['50% cotton', '30% wool', '20% polyester'])
 
 const variants = ref([
@@ -20,17 +18,21 @@ const variants = ref([
   { id: 2235, color: 'blue', image: socksBlueImage, quantity: 0 },
 ])
 
-const addToCart = () => cart.value += 1
+const cart = ref(0)
 //const updateImage = (variantImage) => image.value = variantImage
 const title = computed(() => {
   return brand.value + ' ' + product.value
 })
+
 const image = computed(() => {
   return variants.value[selectedVariant.value].image
 })
+
 const inStock = computed(() => {
   return variants.value[selectedVariant.value].quantity > 0
 })
+
+const addToCart = () => cart.value += 1
 
 const updateVariant = (index) => {
   selectedVariant.value = index
